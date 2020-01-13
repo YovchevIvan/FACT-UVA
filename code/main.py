2323fa7687acc27194ebdcba146205e329f6e4ae
Ivan Yovchev <yovchev23@gmail.com>
2020-01-13 17:27:18 +0100

    Basic refactor of WordEmbedding class

--- code/main.py
+++ code/main.py
@@ -18,7 +18,6 @@ else:
 
 class WordEmbedding:
     def __init__(self, fname):
-        self.desc = fname
         print("*** Reading data from " + fname)
 
         # IMPORTANT: limit argument to be removed after refactoring.
@@ -26,25 +25,22 @@ class WordEmbedding:
         model = gensim.models.KeyedVectors.load_word2vec_format(fname, binary=True, limit=100000)
         self.words = sorted([w for w in model.vocab], key=lambda w: model.vocab[w].index)
         self.vecs = np.array([model[w] for w in self.words], dtype='float32')
-    
-        print(self.vecs.shape)
         self.reindex()
+
         norms = np.linalg.norm(self.vecs, axis=1)
         if max(norms)-min(norms) > 0.0001:
             self.normalize()
 
     def reindex(self):
         self.index = {w: i for i, w in enumerate(self.words)}
-        self.n, self.d = self.vecs.shape
+        self.n, _ = self.vecs.shape
+
         assert self.n == len(self.words) == len(self.index)
-        self._neighbors = None
-        print(self.n, "words of dimension", self.d, ":", ", ".join(self.words[:4] + ["..."] + self.words[-4:]))
 
     def v(self, word):
         return self.vecs[self.index[word]]
 
     def normalize(self):
-        self.desc += ", normalize"
         self.vecs /= np.linalg.norm(self.vecs, axis=1)[:, np.newaxis]
         self.reindex()
 
@@ -54,10 +50,7 @@ class WordEmbedding:
             # store in sorted order: most frequent words at the top
             for i, word in enumerate(self.words):
                 row = self.vecs[i]
-                if binary:
-                    fout.write(to_utf8(word) + b" " + row.tostring())
-                else:
-                    fout.write(to_utf8("%s %s\n" % (word, ' '.join("%f" % val for val in row))))
+                fout.write(to_utf8(word) + b" " + row.tostring())
 
 """
     Additional functions
@@ -67,11 +60,9 @@ def doPCA(pairs, embedding, num_components = 10):
     matrix = []
     for a, b in pairs:
         center = (embedding.v(a) + embedding.v(b))/2
-        matrix.append(embedding.v(a) - center)
-        matrix.append(embedding.v(b) - center)
-    matrix = np.array(matrix)
+        matrix.extend([embedding.v(a) - center, embedding.v(b) - center])
     pca = PCA(n_components = num_components)
-    pca.fit(matrix)
+    pca.fit(np.array(matrix))
     return pca
 
 def drop(u, v):
@@ -129,7 +120,6 @@ if __name__ == "__main__":
     print("gender specific", len(gender_specific_words), gender_specific_words[:10])
 
     E = WordEmbedding(args.embedding_filename)
-    print("done with E")
 
     print("Debiasing...")
     debias(E, gender_specific_words, defs, equalize_pairs)
