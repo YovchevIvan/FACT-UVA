91831836a3e91fb9f1d1be24e72ef40b6b20290e
Ivan Yovchev <yovchev23@gmail.com>
2020-01-15 13:48:30 +0100

    Basic filtering on lowercase

--- code/main.py
+++ code/main.py
@@ -11,167 +11,174 @@ import numpy as np
 import scipy.sparse
 from sklearn.decomposition import PCA
 if sys.version_info[0] < 3:
-	import io
-	open = io.open
+    import io
+    open = io.open
 else:
-	unicode = str
+    unicode = str
 """
-	WordEmbedding Class definition
+    WordEmbedding Class definition
 """
 
 class WordEmbedding:
-	def __init__(self, fname, binary, em_limit):
-		print("*** Reading data from " + fname)
+    def __init__(self, fname, binary, em_limit):
+        print("*** Reading data from " + fname)
 
-		#load model
-		model = gensim.models.KeyedVectors.load_word2vec_format(fname, binary=binary, limit=em_limit)
+        #load model
+        model = gensim.models.KeyedVectors.load_word2vec_format(fname, binary=binary, limit=em_limit)
 
-		assert (model is not None)
+        assert (model is not None)
 
-		self.words = sorted([w for w in model.vocab], key=lambda w: model.vocab[w].index)
-		self.vecs = np.array([model[w] for w in self.words], dtype='float32')
-		self.reindex()
+        self.words = sorted([w for w in model.vocab if self.word_filter(w)], key=lambda w: model.vocab[w].index)
 
-		norms = np.linalg.norm(self.vecs, axis=1)
-		if max(norms)-min(norms) > 0.0001:
-			self.normalize()
+        print("Number of words: ", len(self.words))
 
-	def reindex(self):
-		self.index = {w: i for i, w in enumerate(self.words)}
-		self.n, _ = self.vecs.shape
+        self.vecs = np.array([model[w] for w in self.words], dtype='float32')
+        self.reindex()
 
-		assert self.n == len(self.words) == len(self.index)
+        norms = np.linalg.norm(self.vecs, axis=1)
+        if max(norms)-min(norms) > 0.0001:
+            self.normalize()
 
-	def v(self, word):
-		return self.vecs[self.index[word]]
+    def word_filter(self, word):
+        if word.lower() == word:
+            return word
 
-	def normalize(self):
-		self.vecs /= np.linalg.norm(self.vecs, axis=1)[:, np.newaxis]
-		self.reindex()
+    def reindex(self):
+        self.index = {w: i for i, w in enumerate(self.words)}
+        self.n, _ = self.vecs.shape
 
-	def save_w2v(self, filename, binary=True):
-		with open(filename, 'wb') as fout:
-			fout.write(to_utf8("%s %s\n" % self.vecs.shape))
-			# store in sorted order: most frequent words at the top
-			for i, word in enumerate(self.words):
-				fout.write(to_utf8(word) + b" " + self.vecs[i].tostring())
+        assert self.n == len(self.words) == len(self.index)
+
+    def v(self, word):
+        return self.vecs[self.index[word]]
+
+    def normalize(self):
+        self.vecs /= np.linalg.norm(self.vecs, axis=1)[:, np.newaxis]
+        self.reindex()
+
+    def save_w2v(self, filename, binary=True):
+        with open(filename, 'wb') as fout:
+            fout.write(to_utf8("%s %s\n" % self.vecs.shape))
+            # store in sorted order: most frequent words at the top
+            for i, word in enumerate(self.words):
+                fout.write(to_utf8(word) + b" " + self.vecs[i].tostring())
 
 """
-	Additional functions
+    Additional functions
 """
 
 def doPCA(pairs, embedding, num_components = 10):
-	matrix = []
-	for a, b in pairs:
-		center = (embedding.v(a) + embedding.v(b))/2
-		matrix.extend([embedding.v(a) - center, embedding.v(b) - center])
-	pca = PCA(n_components = num_components)
-	pca.fit(np.array(matrix))
-	return pca
+    matrix = []
+    for a, b in pairs:
+        center = (embedding.v(a) + embedding.v(b))/2
+        matrix.extend([embedding.v(a) - center, embedding.v(b) - center])
+    pca = PCA(n_components = num_components)
+    pca.fit(np.array(matrix))
+    return pca
 
 # def drop(u, v, s):
-#	  return u - v * u.dot(v) * s
+#     return u - v * u.dot(v) * s
 
 def drop(u, v):
-	return u - v * u.dot(v) / v.dot(v)
+    return u - v * u.dot(v) / v.dot(v)
 
 def to_utf8(text, errors='strict', encoding='utf8'):
-	"""Convert a string (unicode or bytestring in `encoding`), to bytestring in utf8."""
-	if isinstance(text, unicode):
-		return text.encode('utf8')
-	# do bytestring -> unicode -> utf8 full circle, to ensure valid utf8
-	return unicode(text, encoding, errors=errors).encode('utf8')
+    """Convert a string (unicode or bytestring in `encoding`), to bytestring in utf8."""
+    if isinstance(text, unicode):
+        return text.encode('utf8')
+    # do bytestring -> unicode -> utf8 full circle, to ensure valid utf8
+    return unicode(text, encoding, errors=errors).encode('utf8')
 
 # def debias(E, gender_specific_words, definitional, equalize):
-#	  gender_direction = doPCA(definitional, E).components_[0]
-#	  # specific_set = set(gender_specific_words)
+#     gender_direction = doPCA(definitional, E).components_[0]
+#     # specific_set = set(gender_specific_words)
 
-#	  scaling = 1/gender_direction.dot(gender_direction)
+#     scaling = 1/gender_direction.dot(gender_direction)
 
-#	  marks = np.zeros(len(E.words), dtype=bool)
-#	  for w in gender_specific_words:
-#		  marks[E.index[w]] = True
+#     marks = np.zeros(len(E.words), dtype=bool)
+#     for w in gender_specific_words:
+#         marks[E.index[w]] = True
 
-#	  i = 0
-#	  for w in E.words:
-#		  if not marks[i]:
-#			  E.vecs[i] = drop(E.vecs[i], gender_direction, scaling)
-#		  i += 1
-#	  E.normalize()
+#     i = 0
+#     for w in E.words:
+#         if not marks[i]:
+#             E.vecs[i] = drop(E.vecs[i], gender_direction, scaling)
+#         i += 1
+#     E.normalize()
 
-#	  # candidates = {x for e1, e2 in equalize for x in [(e1.lower(), e2.lower()),
-#	  #												   (e1.title(), e2.title()),
-#	  #												   (e1.upper(), e2.upper())]}
+#     # candidates = {x for e1, e2 in equalize for x in [(e1.lower(), e2.lower()),
+#     #                                                (e1.title(), e2.title()),
+#     #                                                (e1.upper(), e2.upper())]}
 
-#	  lower = map(lambda x : (x[0].lower(), x[1].lower()), equalize)
-#	  title = map(lambda x : (x[0].title(), x[1].title()), equalize)
-#	  upper = map(lambda x : (x[0].upper(), x[1].upper()), equalize)
+#     lower = map(lambda x : (x[0].lower(), x[1].lower()), equalize)
+#     title = map(lambda x : (x[0].title(), x[1].title()), equalize)
+#     upper = map(lambda x : (x[0].upper(), x[1].upper()), equalize)
 
-#	  for candidates in [lower, title, upper]:
-#		  print(candidates)
-#		  for (a, b) in candidates:
-#			  if (a in E.index and b in E.index):
-#				  y = drop((E.v(a) + E.v(b)) / 2, gender_direction, scaling)
-#				  z = np.sqrt(1 - np.linalg.norm(y)**2)
-#				  if (E.v(a) - E.v(b)).dot(gender_direction) < 0:
-#					  z = -z
-#				  E.vecs[E.index[a]] = z * gender_direction + y
-#				  E.vecs[E.index[b]] = -z * gender_direction + y
-#	  E.normalize()
+#     for candidates in [lower, title, upper]:
+#         print(candidates)
+#         for (a, b) in candidates:
+#             if (a in E.index and b in E.index):
+#                 y = drop((E.v(a) + E.v(b)) / 2, gender_direction, scaling)
+#                 z = np.sqrt(1 - np.linalg.norm(y)**2)
+#                 if (E.v(a) - E.v(b)).dot(gender_direction) < 0:
+#                     z = -z
+#                 E.vecs[E.index[a]] = z * gender_direction + y
+#                 E.vecs[E.index[b]] = -z * gender_direction + y
+#     E.normalize()
 
 def debias(E, gender_specific_words, definitional, equalize):
-	gender_direction = doPCA(definitional, E).components_[0]
-	specific_set = set(gender_specific_words)
-	for i, w in enumerate(E.words):
-		if w not in specific_set:
-			E.vecs[i] = drop(E.vecs[i], gender_direction)
-	E.normalize()
-	candidates = {x for e1, e2 in equalize for x in [(e1.lower(), e2.lower()),
-													 (e1.title(), e2.title()),
-													 (e1.upper(), e2.upper())]}
-	print(candidates)
-	for (a, b) in candidates:
-		if (a in E.index and b in E.index):
-			y = drop((E.v(a) + E.v(b)) / 2, gender_direction)
-			z = np.sqrt(1 - np.linalg.norm(y)**2)
-			if (E.v(a) - E.v(b)).dot(gender_direction) < 0:
-				z = -z
-			E.vecs[E.index[a]] = z * gender_direction + y
-			E.vecs[E.index[b]] = -z * gender_direction + y
-	E.normalize()
+    gender_direction = doPCA(definitional, E).components_[0]
+    specific_set = set(gender_specific_words)
+    for i, w in enumerate(E.words):
+        if w not in specific_set:
+            E.vecs[i] = drop(E.vecs[i], gender_direction)
+    E.normalize()
+    candidates = {x for e1, e2 in equalize for x in [(e1.lower(), e2.lower()),
+                                                     (e1.title(), e2.title()),
+                                                     (e1.upper(), e2.upper())]}
+    print(candidates)
+    for (a, b) in candidates:
+        if (a in E.index and b in E.index):
+            y = drop((E.v(a) + E.v(b)) / 2, gender_direction)
+            z = np.sqrt(1 - np.linalg.norm(y)**2)
+            if (E.v(a) - E.v(b)).dot(gender_direction) < 0:
+                z = -z
+            E.vecs[E.index[a]] = z * gender_direction + y
+            E.vecs[E.index[b]] = -z * gender_direction + y
+    E.normalize()
 
 if __name__ == "__main__":
 
-	parser = argparse.ArgumentParser()
-	parser.add_argument("--em_limit", type=int, default=None, help="number of words to load")
-	parser.add_argument("--bin", type = lambda x : x.lower()=="true" if x.lower()=="true" or x.lower()=="false" else None, default=True, help="Boolean, set to false if using txt file format")
-	parser.add_argument("--i_em", default="../embeddings/GoogleNews-vectors-negative300.bin", help="The name of the embedding")
-	parser.add_argument("--def_fn", help="JSON of definitional pairs", default="../data/definitional_pairs.json")
-	parser.add_argument("--g_words_fn", help="File containing words not to neutralize (one per line)", default="../data/gender_specific_full.json")
-	parser.add_argument("--eq_fn", help="???.bin", default="../data/equalize_pairs.json")
-	parser.add_argument("--o_em", help="???.bin", default="../embeddings/debiased.bin")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--em_limit", type=int, default=50000, help="number of words to load")
+    parser.add_argument("--bin", type = lambda x : x.lower()=="true" if x.lower()=="true" or x.lower()=="false" else None, default=True, help="Boolean, set to false if using txt file format")
+    parser.add_argument("--i_em", default="../embeddings/GoogleNews-vectors-negative300.bin", help="The name of the embedding")
+    parser.add_argument("--def_fn", help="JSON of definitional pairs", default="../data/definitional_pairs.json")
+    parser.add_argument("--g_words_fn", help="File containing words not to neutralize (one per line)", default="../data/gender_specific_full.json")
+    parser.add_argument("--eq_fn", help="???.bin", default="../data/equalize_pairs.json")
+    parser.add_argument("--o_em", help="???.bin", default="../embeddings/debiased.bin")
 
-	args = parser.parse_args()
-	print(args)
+    args = parser.parse_args()
+    print(args)
 
-	with open(args.def_fn, "r") as f:
-		defs = json.load(f)
-	print("definitional", defs)
+    with open(args.def_fn, "r") as f:
+        defs = json.load(f)
+    print("definitional", defs)
 
-	with open(args.eq_fn, "r") as f:
-		equalize_pairs = json.load(f)
+    with open(args.eq_fn, "r") as f:
+        equalize_pairs = json.load(f)
 
-	with open(args.g_words_fn, "r") as f:
-		gender_specific_words = json.load(f)
-	print("gender specific", len(gender_specific_words), gender_specific_words[:10])
+    with open(args.g_words_fn, "r") as f:
+        gender_specific_words = json.load(f)
+    print("gender specific", len(gender_specific_words), gender_specific_words[:10])
 
-	E = WordEmbedding(args.i_em, args.bin, args.em_limit)
+    E = WordEmbedding(args.i_em, args.bin, args.em_limit)
 
-	print("Debiasing...")
-	debias(E, gender_specific_words, defs, equalize_pairs)
+    print("Debiasing...")
+    debias(E, gender_specific_words, defs, equalize_pairs)
 
-	print("Saving to file...")
-	E.save_w2v(args.o_em)
+    print("Saving to file...")
+    E.save_w2v(args.o_em)
 
-	print("\n\nDone!\n")
+    print("\n\nDone!\n")
 
