af2c52539e78eaf21c626f363a87c9b45c4e712c
YovchevIvan <ivan.yovchev@student.uva.nl>
2020-01-23 15:55:10 +0100

    Comments on bash script

--- code/benchmark/scripts/run_test.sh
+++ code/benchmark/scripts/run_test.sh
@@ -3,23 +3,32 @@
 ## get full path to working dir
 full_path=`pwd`
 
+# remove suffix
 suffix="code/benchmark/scripts"
 full_path=${full_path%"$suffix"}
+
+# get path to embeddings
 full_path="${full_path}embeddings"
 
 # declare an array variable
 declare -a vecs=("bias_word2vec" "debiased_word2vec" "bias_glove" "debiased_glove")
 
+# variable to store string
 result=""
+
+# new line character
 NEWLINE=$'\n'
 
 ## now loop through the above array
 iter=0
+
+# for each type of vector
 for i in "${vecs[@]}"
 do
     ## run to get results
     python3 evaluate_on_all.py -f "${full_path}/$i.bin" -o "$i.csv"
 
+    # if its the first iteration
     if [[ "$iter" -eq 0 ]];
     then
         ## get firt line with titles
@@ -45,4 +54,5 @@ do
     iter=$(expr $iter + 1)
 done
 
+# store resulting string to file
 echo "$result" >> result.csv
